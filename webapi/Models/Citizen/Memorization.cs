ac2ea0a71a9c26bdfb6de6f2da5863325837a2b7
Fagun Rain <fagunrain@Faguns-MacBook-Pro.local>
2018-05-09 08:41:43 +0600

    add is_complete property in memorization

--- webapi/Models/Citizen/Memorization.cs
+++ webapi/Models/Citizen/Memorization.cs
@@ -7,6 +7,7 @@ namespace web_api.Models
         public int Id { get; set; }
         public string UserGuId { get; set; }
         public string Name { get; set; }
+        public bool IsCompleted { get; set; }
         public int From { get; set; }
         public int To { get; set; }
         public DateTime UpdatedDate { get; set; }
